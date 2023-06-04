d0cc4ec00c9827e984fc92f364ccdfa169c038bb
Edwin Jimenez <edwinjcasares@gmail.com>
2023-06-04 13:27:39 -0500

    Updated style

--- user.js
+++ user.js
@@ -39,32 +39,33 @@ user_pref("userChrome.decoration.field_border", true);
 user_pref("userChrome.decoration.download_panel", true);
 user_pref("userChrome.decoration.animate", true);
 
+user_pref("userChrome.tab.letters_cleary", true);
 user_pref("userChrome.tab.dynamic_separator", true);
 user_pref("userChrome.tab.close_button_at_hover", true);
 user_pref("userChrome.tab.sound_with_favicons", true);
-user_pref("userChrome.tab.pip", true);
-user_pref("userChrome.tab.container", true);
+user_pref("userChrome.tab.sound_hide_label", true);
+user_pref("userChrome.tab.unloaded", true);
 user_pref("userChrome.tab.crashed", true);
+user_pref("userChrome.tab.pip", true);
 
 user_pref("userChrome.icon.panel_full", true);
-user_pref("userChrome.icon.library", true);
 user_pref("userChrome.icon.panel", true);
+user_pref("userChrome.icon.library", true);
 user_pref("userChrome.icon.menu", true);
+user_pref("userChrome.icon.menu.full", true);
 user_pref("userChrome.icon.context_menu", true);
 user_pref("userChrome.icon.global_menu", true);
 user_pref("userChrome.icon.global_menubar", true);
 
 user_pref("userContent.player.ui", true);
+user_pref("userContent.player.icon", true);
 user_pref("userContent.player.click_to_play", true);
 user_pref("userContent.player.animate", true);
 
-user_pref("userContent.page.field_border", true);
 user_pref("userContent.page.illustration", true);
 user_pref("userContent.page.proton_color", true);
 user_pref("userContent.page.dark_mode", true); // Need proton_color
 user_pref("userContent.page.proton", true); // Need proton_color
-
-user_pref("userChrome.hidden.tabbar", true);
 
 // ** Personal Options ****************************************************************************
 
@@ -87,7 +88,7 @@ user_pref("browser.download.open_pdf_attachments_inline", true);
 user_pref("browser.urlbar.suggest.calculator", true);
 
 // Integrated unit convertor at urlbar
-// user_pref("browser.urlbar.unitConversion.enabled", true);
+user_pref("browser.urlbar.unitConversion.enabled", true);
 
 // Linux: Sidebar Hover fix
 // user_pref("widget.gtk.ignore-bogus-leave-notify", 1);
