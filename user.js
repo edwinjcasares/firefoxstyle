f1ba75e9e81834887230aedbfbc4a9a9a8be629c
Edwin Jimenez <edwinjcasares@gmail.com>
2023-07-07 10:25:05 -0500

    Readdd lepton

--- user.js
+++ user.js
@@ -15,8 +15,74 @@ user_pref("layout.css.backdrop-filter.enabled", true);
 // CSS's `:has()` selector #457
 user_pref("layout.css.has-selector.enabled", true);
 
+// ** Firefox UI Fix - Proton Related Options **********************************************************************
+
+user_pref("userChrome.compatibility.theme", true);
+user_pref("userChrome.compatibility.os", true);
+
+user_pref("userChrome.theme.proton_color", true);
+user_pref("userChrome.theme.proton_chrome", true); // Need proton_color
+user_pref("userChrome.theme.fully_color", true); // Need proton_color
+user_pref("userChrome.theme.fully_dark", true); // Need proton_color
+
+user_pref("userChrome.decoration.cursor", true);
+user_pref("userChrome.decoration.field_border", true);
+user_pref("userChrome.decoration.download_panel", true);
+user_pref("userChrome.decoration.animate", true);
+
+user_pref("userChrome.padding.tabbar_width", true);
+user_pref("userChrome.padding.tabbar_height", true);
+user_pref("userChrome.padding.toolbar_button", true);
+user_pref("userChrome.padding.navbar_width", true);
+user_pref("userChrome.padding.urlbar", true);
+user_pref("userChrome.padding.bookmarkbar", true);
+user_pref("userChrome.padding.infobar", true);
+user_pref("userChrome.padding.menu", true);
+user_pref("userChrome.padding.bookmark_menu", true);
+user_pref("userChrome.padding.global_menubar", true);
+user_pref("userChrome.padding.panel", true);
+user_pref("userChrome.padding.popup_panel", true);
+
+user_pref("userChrome.tab.multi_selected", true);
+user_pref("userChrome.tab.letters_cleary", true);
+user_pref("userChrome.tab.dynamic_separator", true);
+user_pref("userChrome.tab.close_button_at_hover", true);
+user_pref("userChrome.tab.sound_with_favicons", true);
+user_pref("userChrome.tab.sound_hide_label", true);
+user_pref("userChrome.tab.unloaded", true);
+user_pref("userChrome.tab.crashed", true);
+user_pref("userChrome.tab.pip", true);
+
+user_pref("userChrome.icon.panel_full", true);
+user_pref("userChrome.icon.panel", true);
+user_pref("userChrome.icon.library", true);
+user_pref("userChrome.icon.menu", true);
+user_pref("userChrome.icon.menu.full", true);
+user_pref("userChrome.icon.context_menu", true);
+user_pref("userChrome.icon.global_menu", true);
+user_pref("userChrome.icon.global_menubar", true);
+
+user_pref("userContent.player.ui", true);
+user_pref("userContent.player.icon", true);
+user_pref("userContent.player.size", true);
+user_pref("userContent.player.animate", true);
+user_pref("userContent.player.click_to_play", true);
+
+user_pref("userContent.page.field_border", true);
+user_pref("userContent.page.illustration", true);
+user_pref("userContent.page.proton_color", true);
+user_pref("userContent.page.dark_mode", true); // Need proton_color
+user_pref("userContent.page.proton", true); // Need proton_color
+
+user_pref("userChrome.hidden.tabbar", true);
 
 // ** Personal Options ****************************************************************************
+
+// Sharpen Scrolling
+user_pref("mousewheel.min_line_scroll_amount", 10); //        5
+user_pref("general.smoothScroll.mouseWheel.durationMinMS", 80); //       50
+user_pref("general.smoothScroll.currentVelocityWeighting", "0.15"); //   "0.25"
+user_pref("general.smoothScroll.stopDecelerationWeighting", "0.6"); //    "0.4"
 
 // Microphone indicator
 user_pref("privacy.webrtc.legacyGlobalIndicator", false);
@@ -44,7 +110,6 @@ user_pref("browser.urlbar.unitConversion.enabled", true);
 
 // Windows: Ungroup taskbar icons per profile
 user_pref("taskbar.grouping.useprofile", true);
-
 
 // ** Personal Tweaks *****************************************************************************
 
